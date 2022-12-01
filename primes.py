68bb3b0f080289844a8d5cfdc1b946bbf797172c
Pavan-Rana <pavan.rana@kcl.ac.uk>
2022-12-01 16:23:36 +0000

    done

--- primes.py
+++ primes.py
@@ -2,5 +2,18 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError
+        return
     list = []
+    guess = 2
+    while(len(list)<number_of_primes):
+        flag = True
+        for i in range (2,guess):
+            if guess % i == 0:
+                flag = False
+                break
+        if flag:
+            list.append(guess)
+        guess +=1
     return list
